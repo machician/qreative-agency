40202c29c8f4079f8294137e9cfab47c90c46bb7
machician <chielexisigbokwe@gmail.com>
2026-07-20 16:54:48 -0400

    remove metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
-import "@/styles/globals.css";
-import { DarkModeProvider } from '@/providers/DarkModeContext';
+// @ts-ignore: allow importing global CSS without type declarations in this file
+import "../styles/globals.css";
+import { DarkModeProvider } from '../providers/DarkModeContext';
 import { AppProps } from 'next/app';
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: 'QA Labs',
-  description: 'AI, Product Development, SAFE® Stack Audits, GTM Engineering, and Technical Solutions Consulting.',
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
